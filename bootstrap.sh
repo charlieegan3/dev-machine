884f04bda74ee867a6b5547e291f0b6b3bedf498
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-05-28 09:36:49 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ git remote add -t \* -f origin https://github.com/charlieegan3/dotfiles.git
 git fetch origin
 git reset --hard origin/master
 
-sudo apt-get install firefox urxvt tree the_silver_searcher vim 
+sudo apt-get install firefox rxvt-unicode tree silversearcher-ag vim 
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall!
@@ -15,7 +15,5 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --config vi
 
 mkdir ~/Code
