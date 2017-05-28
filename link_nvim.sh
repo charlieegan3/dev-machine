336dba5d8ba0b39e5e0917f3426c7b36fc24c44f
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-05-28 22:53:04 +0100

    Correct indent

--- link_nvim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# The .config folder is cloned in with git
-# mkdir ~/.config
-ln -s ~/.vim ~/.config/nvim
-ln -s ~/.vimrc ~/.config/nvim/init.vim
--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -7,11 +7,11 @@ DEBIAN_FRONTEND=noninteractive
 
 # remove unwanted software
 sudo apt-get remove -y --purge aisleriot cheese dconf-editor gnome-calculator gnome-calendar \
-        gnome-contacts gnome-documents gnome-games gnome-getting-started-docs \
-        gnome-gettings-started-docs gnome-mahjongg gnome-maps gnome-mines \
-        gnome-music gnome-orca gnome-photos gnome-sudoku gnome-user-guide \
-        gnome-user-guide gnome-weather gnome-weather libreoffice* \
-        rhythmbox* simple-scan totem || true
+  gnome-contacts gnome-documents gnome-games gnome-getting-started-docs \
+  gnome-gettings-started-docs gnome-mahjongg gnome-maps gnome-mines \
+  gnome-music gnome-orca gnome-photos gnome-sudoku gnome-user-guide \
+  gnome-user-guide gnome-weather gnome-weather libreoffice* \
+  rhythmbox* simple-scan totem || true
 sudo apt-get clean
 sudo apt-get autoremove
 
@@ -22,10 +22,10 @@ sudo apt-get install -y curl firefox git neovim rxvt-unicode silversearcher-ag t
 
 # configure dotfiles
 if ! [ -d .git ]; then
-        git init .
-        git remote add -t \* -f origin https://github.com/charlieegan3/dotfiles.git
-        git fetch origin
-        git reset --hard origin/master
+  git init .
+  git remote add -t \* -f origin https://github.com/charlieegan3/dotfiles.git
+  git fetch origin
+  git reset --hard origin/master
 fi
 
 # configure nvim
