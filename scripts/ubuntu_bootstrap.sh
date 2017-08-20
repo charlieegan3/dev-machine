7a29867316204a3ef4d06f336ba77bc1710eaf6d
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-08-20 14:35:08 +0100

    Install tmux

--- scripts/ubuntu_bootstrap.sh
+++ scripts/ubuntu_bootstrap.sh
@@ -32,7 +32,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
   wantedPackages=(apt-transport-https awscli direnv ca-certificates \
   chromium-browser curl firefox git neovim silversearcher-ag \
-  software-properties-common tree vim)
+  software-properties-common tmux tree vim)
 
   sudo apt-get update >> /dev/null
   for package in "${wantedPackages[@]}"
