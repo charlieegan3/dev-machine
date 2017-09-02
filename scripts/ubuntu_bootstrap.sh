6c56da6b0cf76acd8660e9a3d266f283813dbad0
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-09-02 15:12:50 +0100

    Add google chrome package key & repo

--- scripts/ubuntu_bootstrap.sh
+++ scripts/ubuntu_bootstrap.sh
@@ -30,6 +30,9 @@ read -p "Install packages? y/n" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
   wantedPackages=(apt-transport-https awscli direnv ca-certificates \
   google-chrome-stable curl firefox gconf2 git silversearcher-ag \
   redshift software-properties-common tree vim-gnome)
