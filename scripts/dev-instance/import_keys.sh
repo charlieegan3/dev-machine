fd610ae3fce05c2f48829d22948392c89c5c48d0
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-10-17 05:23:57 +0000

    Add script to import gpg keys

---
+++ scripts/dev-instance/import_keys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+set -e
+
+curl https://github.com/charlieegan3.keys > /home/charlieegan3/.ssh/authorized_keys
+
+mkdir -p gpg_keys
+
+gsutil cp gs://charlieegan3-gpg-keys/* gpg_keys/
+
+for f in gpg_keys/*
+do
+  gpg --import "$f"
+done
+
+rm -rf gpg_keys
