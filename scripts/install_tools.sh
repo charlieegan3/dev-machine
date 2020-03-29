4bc27dd3d25604a64f76ead19e40b10bd0abcb10
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-03-29 17:00:08 +0000

    Correct gpg package name

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -3,7 +3,7 @@
 set -exo pipefail
 
 apt-get update
-apt-get install -y silversearcher-ag tmux direnv jq tree mosh git make curl gpg gpg2 \
+apt-get install -y silversearcher-ag tmux direnv jq tree mosh git make curl gpg gnupg2 \
   software-properties-common unzip vim python3-pip apt-transport-https ca-certificates \
   build-essential
 
