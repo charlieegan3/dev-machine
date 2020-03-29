6028fe39f14208b406ca475137464abaf2cf4989
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-03-29 09:46:31 +0000

    Install docker with packer instead
    Was having issues with it not working with systemctl in docker build

---
+++ scripts/install_docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker charlieegan3
+docker pull ubuntu:latest
