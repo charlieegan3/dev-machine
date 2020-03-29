c876045da0feabfc48eb0fee135d4c2ecec7c13c
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-03-29 10:05:26 +0000

    Add -y flag for docker install

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -2,8 +2,8 @@
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce
+sudo apt-get update
+sudo apt-get install -y docker-ce
 sudo systemctl status docker
 sudo usermod -aG docker charlieegan3
 docker pull ubuntu:latest
