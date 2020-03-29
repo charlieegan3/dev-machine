5e3482f3efd03d62c72386d5007577c07351a241
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-03-29 17:22:58 +0000

    Make message use the right time

--- scripts/install_login_notification.sh
+++ scripts/install_login_notification.sh
@@ -3,9 +3,11 @@
 : "${PUSHOVER_TOKEN:?Need to set PUSHOVER_TOKEN non-empty}"
 : "${PUSHOVER_USER:?Need to set PUSHOVER_USER non-empty}"
 
+message='Login at $(date)'
+
 cat << EOF > /home/$USERNAME/.ssh/rc
 curl -X POST https://api.pushover.net/1/messages.json \
-  -d message="Login at $(date)" \
+  -d message="$message" \
   -d token="$PUSHOVER_TOKEN" \
   -d user="$PUSHOVER_USER"
 EOF
