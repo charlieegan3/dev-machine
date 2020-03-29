5833a7d50fa131b760c15b5ce65a9a2e45c02b8b
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-03-29 20:43:26 +0000

    Allow web access

--- scripts/install_ufw.sh
+++ scripts/install_ufw.sh
@@ -4,6 +4,11 @@ set -exo pipefail
 
 ufw default deny incoming
 ufw default allow outgoing
+
 ufw allow ssh
+
 ufw allow 60000:61000/udp # mosh
+ufw allow 3000:3100/tcp # web
+ufw allow 8000:8100/tcp # web
+
 ufw enable
