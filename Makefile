3eb9dbdf7893777f7cf6eceed2cf6cf45a187fcc
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-03-29 11:38:45 +0000

    Complete config in packer alone

--- Makefile
+++ Makefile
@@ -1,8 +1,3 @@
 build:
-	sudo docker build \
-	  --build-arg="password=example" \
-	  --build-arg="github_token=example" \
-	  --build-arg="id_rsa=MQ0=" \
-	  --build-arg="gpg_pub=$$(cat examples/example.public | base64 -w 0)" \
-	  --build-arg="gpg_priv=$$(cat examples/example.private | base64 -w 0)" \
-	  --tag="dev-machine:latest" .
+	packer validate packer.json
+	packer build packer.json
---
+++ scripts/config_dotfiles.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# setup dotfiles
+cd /home/$USERNAME
+find dotfiles -maxdepth 1 -mindepth 1 -exec mv {} . \;
+rm -r dotfiles
+ls -al
+chown -R $USERNAME:$USERNAME /home/$USERNAME
+
+sudo -i -u $USERNAME bash << EOF
+# install vim-plug
+curl -fLo /home/$USERNAME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
+
+# install go toolchain
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/Code/go
+vim main.go +GoInstallBinaries +qall # main.go to load the plugin
+go get -u github.com/visualfc/gocode
+pip3 install --upgrade neovim
+EOF
