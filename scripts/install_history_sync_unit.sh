15b19ed01d86d0411e848bec16bb1d07afc003da
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-04-01 23:00:36 +0000

    Install history sync script

---
+++ scripts/install_history_sync_unit.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+: "${USERNAME:?Need to set USERNAME non-empty}"
+
+export REPO_PATH=/home/$USERNAME/Code/secrets-history
+
+cat << EOF > /etc/systemd/system/inactive-termination.service
+[Unit]
+Description=History Sync Unit
+After=history-sync.service
+
+[Service]
+Type=simple
+ExecStart=/etc/history-sync.sh
+Restart=Always
+User=$USERNAME
+EOF
+
+cat << EOF > /etc/history-sync.sh
+#!/usr/bin/env bash
+
+make -C $REPO_PATH sync
+EOF
+
+chmod +x /etc/history-sync.sh
+
+sudo -i -u $USERNAME bash << EOF
+git clone https://github.com/charlieegan3/secrets-history $REPO_PATH
+EOF
