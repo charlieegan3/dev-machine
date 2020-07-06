696ba3d0039105fd1cd1837aece97e051e7fc377
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-07-06 10:37:34 +0100

    Uninstall fasd

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -10,14 +10,6 @@ apt-get install -y silversearcher-ag tmux direnv jq tree mosh git make curl gpg 
   build-essential postgresql-client libpq5 libpq-dev git-crypt python3-setuptools \
   libmagick++-dev
 
-# fasd
-curl -o fasd.zip -L https://github.com/clvv/fasd/zipball/1.0.1 && \
-  unzip fasd.zip && \
-  cd clvv-fasd-4822024 && \
-  make install && \
-  cd .. && \
-  rm -rf clvv-fasd-4822024 fasd.zip
-
 # rg
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb && \
   dpkg -i ripgrep_0.10.0_amd64.deb && \
